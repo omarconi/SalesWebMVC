4c20d75f435527754beaa6fcd4bfb381e748a920
Marconi <54293012+omarconi@users.noreply.github.com>
2021-11-11 01:21:54 -0300

    Project created

---
+++ cmd-git-assoc.bat
@@ -0,0 +1,5 @@
+cd\pasta\solution
+git init
+git remote add origin https://github.com/omarconi/SalesWebMVC.git //vínculo do projeto git com a pasta atual
+git pull origin master //atualiza projeto local com base no github
+
